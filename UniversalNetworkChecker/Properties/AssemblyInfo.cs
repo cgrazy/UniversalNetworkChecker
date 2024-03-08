e640882926dd7750da7762726566fc29c6642eb2
cgrazy <66645171+cgrazy@users.noreply.github.com>
2024-03-08 21:49:28 +0100

    Update AssemblyInfo.cs

--- UniversalNetworkChecker/Properties/AssemblyInfo.cs
+++ UniversalNetworkChecker/Properties/AssemblyInfo.cs
@@ -5,15 +5,6 @@ using System.Security.Cryptography.X509Certificates;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("UniversalNetworkChecker")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
