b014d464f01e25685cf68da8ced226002ba70cea
cgrazy <66645171+cgrazy@users.noreply.github.com>
2024-03-08 21:50:35 +0100

    Update AssemblyInfo.cs

--- UniversalNetworkChecker/Properties/AssemblyInfo.cs
+++ UniversalNetworkChecker/Properties/AssemblyInfo.cs
@@ -9,8 +9,6 @@ using System.Security.Cryptography.X509Certificates;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.0.1")]
-
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
