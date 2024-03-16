a685fcea8d1e197ab33703700a7884e4eaa171a6
cgrazy <66645171+cgrazy@users.noreply.github.com>
2024-03-16 23:20:20 +0100

    Bump version

--- UniversalNetworkChecker/Properties/AssemblyInfo.cs
+++ UniversalNetworkChecker/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
