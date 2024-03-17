6a2495c782759127af7988be557690e6a4daa49c
cgrazy <66645171+cgrazy@users.noreply.github.com>
2024-03-17 21:45:29 +0100

    Improve output

--- UniversalNetworkChecker/UniversalNetworkCheckerResult.cs
+++ UniversalNetworkChecker/UniversalNetworkCheckerResult.cs
@@ -44,8 +44,16 @@
             failureTimes = string.Format($"{Environment.NewLine} Failure times: {GetFailureTimes()}");
         }
 
-        double percentageLoss = ((double)PingFailureTimes.Count / (double)PingSuccess.Count) * (double)100;
-        return string.Format($"hostname: {Hostname}, IP: {IP},  avg: {string.Format("{0:F2}", PingRoundtripTime.Average())} ms , Result: {lastTwentyResults} ({string.Format("{0:F2}", percentageLoss)} % loss) {failureTimes}");
+        double percentageLoss = DataCalculator.GetPercentageValueOf(PingFailureTimes.Count, PingSuccess.Count);
+
+        double avg = PingRoundtripTime.Average();
+        double min = PingRoundtripTime.Min<long>();
+        double max = PingRoundtripTime.Max<long>();
+        double median = DataCalculator.GetMedianOfList(PingRoundtripTime);
+
+        string minMedianMax = string.Format("(min/median/max: {0:F2} ms/{1:F2} ms/{2:F2} ms)", min, median, max);
+
+        return string.Format($"hostname: {Hostname}, IP: {IP},  avg: {string.Format("{0:F2}", avg)} ms {minMedianMax}, Result: {lastTwentyResults} ({string.Format("{0:F2}", percentageLoss)} % loss) {failureTimes}");
     }
 
     private string GetFailureTimes()
@@ -68,5 +76,25 @@
         }
 
         return resultString;
+    }
+}
+
+internal class DataCalculator
+{
+    internal static double GetPercentageValueOf(int failure, int all)
+    {
+        return (double)failure / (double)all * 100;
+    }
+
+    internal static double GetMedianOfList(List<long> values)
+    {
+        values.Sort();
+
+        int size = values.Count;
+        int mid = size / 2;
+
+        double median = (size % 2 != 0) ? (double)values[mid] : ((double)values[mid] + (double)values[mid - 1]) / 2;
+
+        return median;
     }
 }
